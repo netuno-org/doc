aa43942944b3960516b5adeeabb3a6875c1168ff
Eduardo Velasques <eduveks@gmail.com>
2025-05-22 14:47:27 +0100

    Website title has been optimized.

--- src/pages/index.js
+++ src/pages/index.js
@@ -46,8 +46,8 @@ export default function Home() {
     const {siteConfig} = useDocusaurusContext();
     return (
         <Layout
-            title={`Hello from ${siteConfig.title}`}
-            description="Description will go into a meta tag in <head />">
+            title={`Netuno`}
+            description="Netuno general documentation.">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
