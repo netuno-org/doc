d077b892d4a24580decbf126127858922ad299cc
Eduardo Velasques <eduveks@gmail.com>
2025-08-22 20:34:51 +0100

    Config additional languages for the code blocks.

--- docusaurus.config.sample.js
+++ docusaurus.config.sample.js
@@ -182,6 +182,7 @@ const config = {
       prism: {
         theme: prismThemes.github,
         darkTheme: prismThemes.dracula,
+        additionalLanguages: ['ruby', 'groovy']
       },
       algolia: {
         // The application ID provided by Algolia
