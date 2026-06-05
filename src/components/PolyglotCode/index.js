13bb6ef1a99ae68fc82e6ab39c96975827196519
Eduardo Velasques <eduveks@gmail.com>
2026-06-05 20:23:57 +0100

    The Polyglot Code component was refactored.

--- src/components/PolyglotCode/index.js
+++ src/components/PolyglotCode/index.js
@@ -4,6 +4,17 @@ import CodeBlock from '@theme/CodeBlock';
 
 function PolyglotCode({file, codes}) {
     const langs = ['javascript', 'python', 'ruby', 'kotlin', 'groovy'];
+    for (const code of codes) {
+        code.code = code.code.substring(code.code.indexOf('\n') + 1).trimEnd().split('\n')
+            .reduce(({spaces, source}, line, index) => {
+                    if (index === 0) {
+                        spaces = line.match(/^\s*/)[0].length;
+                    }
+                    source += line.substring(spaces) + '\n';
+                    return {spaces, source};
+                }, {spaces: 0, source: ''}
+            ).source;
+    }
     for (const l of langs) {
         if (!codes.find(({lang}) => l === lang)) {
             let code = null;
@@ -14,12 +25,29 @@ function PolyglotCode({file, codes}) {
             }
             if (!code || Object.keys(code).length === 0) {
                 code = {...codes[0]};
+                code.code = code.code.split('\n')
+                    .reduce(({spaces, indents, source}, line, index) => {
+                        const lineSpaces = line.search(/\S/);
+                        if (lineSpaces > spaces) {
+                            indents++;
+                        } else if (lineSpaces < spaces) {
+                            indents--;
+                        }
+                        line = line.trim();
+                        for (let i = 0; i < indents; i++) {
+                            line = '    ' + line;
+                        }
+                        source += line + '\n';
+                        spaces = lineSpaces;
+                        return {spaces, indents, source};
+                    }, {spaces: 0, indents: 0, source: ''}
+                ).source;
             }
             code.lang = l;
             if (l === 'ruby') {
-                code.code = code.code.replaceAll(/([\[(,\s=ˆ])True([\]),\s$])/gm, "$1true$2");
+                code.code = code.code.replaceAll(/([\[(,\s=ˆ])(True|False)([\]),\s$])/gm, "$1$2$3");
             } else if (l === 'python') {
-                code.code = code.code.replaceAll(/([\[(,\s=ˆ])true([\]),\s$])/gm, "$1True$2");
+                code.code = code.code.replaceAll(/([\[(,\s=ˆ])(true|false)([\]),\s$])/gm, "$1$2$3");
             }
             codes.push(code);
         }
@@ -50,6 +78,9 @@ function PolyglotCode({file, codes}) {
         groovy: '// ',
     };
     for (const code of codes) {
+        if (code.lang !== 'javascript') {
+            code.code = code.code.replaceAll(/;(\s+)\/\/\s+/gm, "$1"+ langsComment[code.lang]);
+        }
         code.code = code.code.replaceAll(/(\s+)\/\/\s+/gm, "$1"+ langsComment[code.lang]);
     }
     return (
@@ -75,17 +106,7 @@ function PolyglotCode({file, codes}) {
                             language={lang}
                             title={title}
                             showLineNumbers>
-                            {code.substring(code.indexOf('\n') + 1, code.lastIndexOf('\n'))
-                                .split('\n')
-                                .reduce(({spaces, source}, line, index) => {
-                                        if (index === 0) {
-                                            spaces = line.match(/^\s*/)[0].length;
-                                        }
-                                        source += line.substring(spaces) + '\n';
-                                        return {spaces, source};
-                                    }, {spaces: 0, source: ''}
-                                ).source
-                            }
+                            {code}
                         </CodeBlock>
                     );
                     if (lang === 'javascript') {
